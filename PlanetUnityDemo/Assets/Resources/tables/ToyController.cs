9e3caccdda1712338e223a881c091cf0215111de
KittyMac <rocco@chimerasw.com>
2014-06-11 13:26:36 -0400

    updates sample to show multiple row heights

--- PlanetUnityDemo/Assets/Resources/tables/ToyController.cs
+++ PlanetUnityDemo/Assets/Resources/tables/ToyController.cs
@@ -19,33 +19,40 @@ public class ToyController : MonoBehaviour, IPUCode{
 
 	public PUTable ToyTable;
 
+	string randomLoremIpsum()
+	{
+		string lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		int length = UnityEngine.Random.Range (0, lorem.Length - 20);
+		return lorem.Substring (0, 20 + length);
+	}
+
 	void Start () {
 		List<object> myToys = new List<object> ();
 
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
-		myToys.Add (new Toy ("Robot", "tables/robot", "This is a cool, cool robot!"));
-		myToys.Add (new Toy ("Doll", "tables/doll", "This is a cool, cool doll!"));
-		myToys.Add (new Toy ("Airplane", "tables/airplane", "This is a cool, cool airplane!"));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
+		myToys.Add (new Toy ("Robot", "tables/robot", randomLoremIpsum()));
+		myToys.Add (new Toy ("Doll", "tables/doll", randomLoremIpsum()));
+		myToys.Add (new Toy ("Airplane", "tables/airplane", randomLoremIpsum()));
 
 		ToyTable.SetObjectList (myToys);
 		ToyTable.ReloadTable ();
--- PlanetUnityDemo/Assets/Resources/tables/ToyTableCell.cs
+++ PlanetUnityDemo/Assets/Resources/tables/ToyTableCell.cs
@@ -8,6 +8,7 @@ public class ToyTableCell : PUTableCell {
 	public PULabel ToyName;
 	public PULabel ToyDesc;
 	public PUImage ToyIcon;
+	public PUColor Line;
 
 	public override string XmlPath() {
 		return "tables/toy";
@@ -24,5 +25,14 @@ public class ToyTableCell : PUTableCell {
 		ToyName.LoadTextString (info.name);
 		ToyDesc.LoadTextString (info.description);
 		ToyIcon.LoadImageResource (info.image);
+
+		// Wouldn't it be cool to support multiple row heights?  Its easier than you'd think.
+		// Just remember at this level we're in unity coords, so 0 is the bottom of the cell.
+		puGameObject.bounds.h = ToyDesc.textMesh.renderer.bounds.size.y + ( puGameObject.bounds.h - ToyDesc.bounds.y );
+
+		// We want the line to attach to the bottom of the cell
+		Vector2 pos = Line.gameObject.transform.localPosition;
+		pos.y -= ToyDesc.textMesh.renderer.bounds.size.y ;
+		Line.gameObject.transform.localPosition = pos;
 	}
 }
